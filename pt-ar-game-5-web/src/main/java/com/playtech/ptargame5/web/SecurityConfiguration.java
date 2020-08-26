f3bdbc3d651434c87cd64aa9fda43e9cf0f45de3
lanbotdevman <lanbotdevman@gmail.com>
2020-08-26 18:24:41 +0300

    Revamped the security implementation.
    Reference for the implementation:
    https://docs.spring.io/spring-security/site/docs/5.2.1.RELEASE/reference/htmlsingle/#multiple-httpsecurity

--- pt-ar-game-5-web/src/main/java/com/playtech/ptargame5/web/SecurityConfiguration.java
+++ pt-ar-game-5-web/src/main/java/com/playtech/ptargame5/web/SecurityConfiguration.java
@@ -1,40 +1,65 @@
 package com.playtech.ptargame5.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
+public class SecurityConfiguration {
+
 	@Autowired
 	private ApplicationProperties conf;
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		
-		auth
-				.inMemoryAuthentication()
-				.withUser("guest").password(encoder.encode(conf.getGuestPassword())).roles("guest-role").and().passwordEncoder(encoder)
-				.withUser("private").password(encoder.encode(conf.getApiPassword())).roles("api-role").and().passwordEncoder(encoder);
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public UserDetailsService userDetailsService() throws Exception {
 
-		http.authorizeRequests().antMatchers("/bg-leaderboard.jpg").permitAll();
+		BCryptPasswordEncoder encoder = passwordEncoder();
+
+		UserBuilder users = User.builder();
 		
-		http.csrf().disable();
-		http.formLogin()
-				.loginPage("/login.html")
-				.permitAll();
-		
-		http.authorizeRequests().antMatchers("/*").hasRole("guest-role");
-		http.antMatcher("/api/private/**").authorizeRequests().anyRequest().hasRole("api-role").and().httpBasic();
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(users.username("guest").password(encoder.encode(conf.getGuestPassword())).roles("guest-role").build());
+		manager.createUser(users.username("private").password(encoder.encode(conf.getApiPassword())).roles("api-role").build());
+
+		return manager;
+	}
+
+	@Configuration
+	@Order(1)
+	public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.antMatcher("/api/private/**")
+					.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().hasRole("api-role"))
+					.httpBasic();
+		}
+	}
+
+	@Configuration
+	public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+			http.authorizeRequests().antMatchers("/bg-leaderboard.jpg").permitAll();
+			http
+					.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+					.formLogin().loginPage("/login.html").permitAll();
+		}
 	}
 }
